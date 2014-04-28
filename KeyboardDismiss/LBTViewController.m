d2ea332ab4c392fe5abc4a3e830bb7185ca92586
Brock Boland <brock@brockboland.com>
2014-04-27 21:38:46 -0500

    Demo

--- KeyboardDismiss/LBTViewController.m
+++ KeyboardDismiss/LBTViewController.m
@@ -8,22 +8,11 @@
 
 #import "LBTViewController.h"
 
-@interface LBTViewController ()
-
-@end
-
 @implementation LBTViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)didEndOnExit:(id)sender {
+  // Do nothing. Keyboard dismisses anyway.
 }
 
 @end
